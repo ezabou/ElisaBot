dd63d817472246a7afa5ed57ad659e30c6371193
ezabou <ezabou@gmail.com>
2018-05-04 08:09:16 +0200

    Add files via upload

--- heroku/index.js
+++ heroku/index.js
@@ -64,6 +64,36 @@ function createWebhook(smoochCore, target) {
         });
 }
 
+// Zab
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+// End Zab
+
 // Create a webhook if one doesn't already exist
 if (process.env.SERVICE_URL) {
     const target = process.env.SERVICE_URL.replace(/\/$/, '') + '/webhook';
