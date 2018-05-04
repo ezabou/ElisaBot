3795e5599790cc36d7ab06c9110b73762c807e92
ezabou <ezabou@gmail.com>
2018-05-04 08:21:45 +0200

    Add files via upload

--- heroku/index.js
+++ heroku/index.js
@@ -38,39 +38,6 @@ const store = new SmoochApiStore({
 });
 const lock = new MemoryLock();
 
-// Zab
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
-
-app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
-app.use(bodyParser.json())
-
-// Index route
-app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-})
-
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'test_token') {
-        res.send(req.query['hub.challenge'])
-    }
-    res.send('Error, wrong token')
-})
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
-
-// End Zab
 
 function createWebhook(smoochCore, target) {
     return smoochCore.webhooks.create({
